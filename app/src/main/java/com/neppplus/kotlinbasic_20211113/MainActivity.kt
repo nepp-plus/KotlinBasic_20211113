15c8c9a5be5d781999c744d709786abe146e1bd9
nepp-plus <nepp.plus@gmail.com>
2021-11-13 10:48:04 +0900

    버튼 이벤트 처리  - 실행 내용 : 로그 찍어보기

--- app/src/main/java/com/neppplus/kotlinbasic_20211113/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20211113/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.kotlinbasic_20211113
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,6 +11,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLog.setOnClickListener {
+
+//            이 내부의 코드는, 로그 찍기 버튼을 누를때마다 실행됨.
+
+            Log.d( "메인화면", "로그 찍기 버튼 누름" )
+
+        }
 
     }
 
